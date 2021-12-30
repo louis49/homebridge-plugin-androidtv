5a9d2e68d4c5bf07e76037b6e2bc40d17cb3c030
Desnos Benoît <desnos.benoit@gmail.com>
2021-12-30 13:59:14 +0100

    Fix others path

--- homebridge/admin/static/index.js
+++ homebridge/admin/static/index.js
@@ -24,7 +24,7 @@ const Devices = {
         },
         send_code: function (device){
             console.log('sendCode ' + device.host + ' ' + this.code);
-            axios.put('http://localhost:8181/api/devices/' + device.host + '/secret', {code:this.code}).then((response) => {
+            axios.put('api/devices/' + device.host + '/secret', {code:this.code}).then((response) => {
                 this.devices[device.host] = response.data;
                 this.code = "";
                 console.log('Code received', response);
@@ -32,19 +32,19 @@ const Devices = {
         },
         start: function (device){
             console.log('Start ' + device.host);
-            axios.put('http://localhost:8181/api/devices/' + device.host + '/start', {code:this.code}).then((response) => {
+            axios.put('api/devices/' + device.host + '/start', {code:this.code}).then((response) => {
                 this.devices[device.host] = response.data;
             });
         },
         power: function (device){
             console.log('Send Power ' + device.host);
-            axios.get('http://localhost:8181/api/devices/' + device.host + '/power').then((response) => {
+            axios.get('api/devices/' + device.host + '/power').then((response) => {
 
             });
         },
         set_type: function (device){
             console.log('Send Type ' + device.type);
-            axios.put('http://localhost:8181/api/devices/' + device.host + '/type', {type:device.type}).then((response) => {
+            axios.put('api/devices/' + device.host + '/type', {type:device.type}).then((response) => {
                 this.devices[device.host] = response.data;
                 console.log('Type changed', response);
             });
